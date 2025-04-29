3fd4f60a3864afdfdfdbe1abeb168b56b6272c75
Rupesh040899 <rupesh.mali0408@gmail.com>
2025-04-29 12:04:36 +0530

    Update SearchBar.jsx

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -38,7 +38,7 @@ export default function SearchBar({ list, filterList }) {
           size="large"
           startIcon={<SearchIcon />}
           label="Search"
-          id="searchBtn" 
+          
           sx={{ py: "15px", px: 8, flexShrink: 0 }}
           disableElevation
         >
